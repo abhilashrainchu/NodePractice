d78399eb79ce47080f255c4cc5e83155023f3e4f
Abhilash Rainchu <abhilashrainchu@gmail.com>
2018-04-15 12:57:33 -0500

    i added nodemon function for restarting application automatically when changes occur

--- os.js
+++ os.js
@@ -2,23 +2,22 @@ console.log("Application Started")
 
 
 const _ = require('lodash');
-const os = require('os');
+
 const fs = require('fs');
 const notes = require('./notes.js')
-var  user = os.userInfo();
+
 var unique = _.uniq([2,1,2,1,3,4,3,4]);
 var sub = _.without([1,2,3,4],1,2,3);
 var array = [1];
-var other = _.concat(array,2,3,4,5,6,7,8,9,10);
+var other = _.concat(array,2,3,4,5,5,5,5,5,5,);
 var split = _.chunk(['a','b','c','d','e','f','g','h'],2);
 
 
-//console.log(user);
-//var res = notes.addNote();
 
-//console.log(_.isString(True));
+
+console.log(_.isString(True));
 console.log(_.isString("Abhilash"));
-//console.log(_.isString("Abhilash"));
+
 console.log(unique);
 console.log(sub);
 console.log(other);
