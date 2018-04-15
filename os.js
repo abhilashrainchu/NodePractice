57b32253d43af4f17452369374d46286f1a30779
Abhilash Rainchu <abhilashrainchu@gmail.com>
2018-04-15 16:22:13 -0500

    i added a code which accept input from user

--- os.js
+++ os.js
@@ -7,5 +7,22 @@ const fs = require('fs');
 const notes = require('./notes.js')
 
 
+var command = process.argv[2];
+console.log('command:',command);
+if(command == 'add')
+{
+    console.log('adding new note');
+
+}
+else if (command == 'list')
+{
+    console.log('listing all the notes');
+}
+else
+{
+    console.log("no command found")
+}
+console.log(process.argv);
+
 
 
