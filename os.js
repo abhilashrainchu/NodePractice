d4345b3b8c92d6d442dd3be6922d7fc3f0be6512
Abhilash Rainchu <abhilashrainchu@gmail.com>
2018-04-15 11:45:23 -0500

    i started using lodash function

--- os.js
+++ os.js
@@ -1,20 +1,24 @@
 console.log("Application Started")
 
 
-
+const _ = require('lodash');
 const os = require('os');
 const fs = require('fs');
 const notes = require('./notes.js')
 var  user = os.userInfo();
 
-console.log(user);
-var res = notes.addNote();
+//console.log(user);
+//var res = notes.addNote();
 
-console.log(res);
+//console.log(_.isString(True));
+console.log(_.isString("Abhilash"));
+console.log(_.isString("Abhilash"));
 
-console.log('result', notes.addition(6, 6));
+//console.log(res);
 
-console.log('solution', notes.subs(9, 6));
+//console.log('result', notes.addition(6, 6));
+
+//console.log('solution', notes.subs(9, 6));
 //fs.appendFile("osfs.txt", 'hello' + user.username + 'you are ${notes.age}');
 
 
