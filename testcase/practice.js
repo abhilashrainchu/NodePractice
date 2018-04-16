4b6f5500a8493632cd0323b4e63add264def0878
Abhilash Rainchu <abhilashrainchu@gmail.com>
2018-04-16 17:53:19 -0500

    i added a program which convert object to string and string to object

--- testcase/practice.js
+++ testcase/practice.js
@@ -7,11 +7,32 @@
 //console.log("name", stringobj);
 //console.log(typeof stringobj);
 
-var personString   = '{"name":"Abhilash","age":"25"}';
+//var personString   = '{"name":"Abhilash","age":"25"}';
 
-var person = JSON.parse(personString);
+//var person = JSON.parse(personString);
 
-console.log(person);
-console.log(typeof person);
+//console.log(person);
+//console.log(typeof person);cou
+
+const  fs = require('fs');
+
+var a = {
+    name: "Abhilash",
+    course: "Database"
+}
+console.log(typeof a);
+var astring = JSON.stringify(a);
+console.log(astring);
+console.log(typeof astring);
+fs.writeFileSync('notes.json', astring);
+
+var noteString = fs.readFileSync('notes.json', astring);
+
+var note = JSON.parse(noteString);
+console.log(note);
+console.log(typeof note);
+
+
+
 
 
