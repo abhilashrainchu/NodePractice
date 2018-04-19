97d36db59ac1c9760e56c1d4c569f8e4c221993a
Abhilash Rainchu <abhilashrainchu@gmail.com>
2018-04-19 10:53:14 -0500

    code to accept user input and fetch weather details based on  user input

--- weather-app/playground/app.js
+++ weather-app/playground/app.js
@@ -1,9 +1,26 @@
 const request = require('request');
+const args  = require('yargs');
+const argv = args
+    .options({
+       a: {
+           demand: true,
+           alias: 'address',
+           describe: 'address to fetch wather for ',
+           string: true
+
+       }
+    })
+    .help()
+   // .alias('help', h )
+    .argv;
+console.log(argv);
+var encodeaddress = encodeURIComponent(argv.a);
 
 
-request({url: "https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombardstreet%20philadelphia",
+request({url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeaddress}`,
     JSON: true }, (error,response,body) => {
   console.log(`address: ${JSON.parse(body).results[0].formatted_address}`);
   console.log(`latitude: ${JSON.parse(body).results[0].geometry.location.lat}`);
     console.log(`longitude: ${JSON.parse(body).results[0].geometry.location.lng}`);
+    console.log(`address: ${JSON.parse(body).results[0].formatted_address}`);
     });
