1e252914e6a793a36a2182150c3a910c81e69079
Abhilash Rainchu <abhilashrainchu@gmail.com>
2018-04-19 15:34:04 -0500

    code for detecting live weather app using the forecast link and print error message if there is any error

--- weather-app/playground/app.js
+++ weather-app/playground/app.js
@@ -26,15 +26,24 @@ else{
 
 //e7e7a4967053b50b33b6f7e686a94b2 */
 /*var request = require('request');
- request({
-     url: 'https://api.darksky.net/forecast/e7e7a4967053b50b33b6f7e686a94b2e/35.7085641,97.4165053', JSON: true }, (error, response, body) => {
-         console.log(JSON.parse(body).currently.temperature);
-
-     });*/
-
-var request = require('request');
 request('https://api.darksky.net/forecast/e7e7a4967053b50b33b6f7e686a94b2e/35.7085641,97.4165053', function (error, response, body) {
     //console.log('error:', error); // Print the error if one occurred
     //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:',JSON.parse(body).currently.temperature ); // Print the HTML for the Google homepage.
-});
+});*/
+var request = require('request');
+ request({
+     url: 'https://api.darksky.net/forecast/e7e7a4967053b50b33b6f7e686a94b2e/35.7085641,97.4165053', JSON: true }, (error, response, body) => {
+        if (error){
+            console.log("unable to connect to servers");
+        }
+        else if(response.statusCode === 400) {
+            console.log('unable to fetch error');
+
+        }
+        else if (response.statusCode === 200)
+         console.log(JSON.parse(body).currently.temperature);
+
+     });
+
+
