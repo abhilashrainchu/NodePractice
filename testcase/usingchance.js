31b5482bc5d9d5d04de1585656d0e5ddb9a092e5
Abhilash Rainchu <abhilashrainchu@gmail.com>
2018-04-18 22:31:03 -0500

    added junk

--- testcase/usingchance.js
+++ testcase/usingchance.js
@@ -1,15 +1,6 @@
-const Chance = require('chance'),
-    chance = new Chance();
 
-//console.log(chance.zip());
-
-var params = {
-    formatted: false,
-    country: 'fr'
-};
-console.log(chance.phone(params));
+abhilahs a
+sd
 
 
-
-var repeat = require('repeat-string');
-console.log(repeat('A', 5));
+asdajklhsd iasd in feature 1
