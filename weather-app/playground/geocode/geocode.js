a579b4ba1652102eb2cf3b6ee3900ad2c3ee816b
Abhilash Rainchu <abhilashrainchu@gmail.com>
2018-04-19 14:13:08 -0500

    added call back function

--- weather-app/playground/geocode/geocode.js
+++ weather-app/playground/geocode/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-var geocodeAddress  = (address) => {
+var geocodeAddress  = (address,callback) => {
 
     var encodeaddress = encodeURIComponent(address);
 
@@ -10,19 +10,22 @@ var geocodeAddress  = (address) => {
         JSON: true
     }, (error, response, body) => {
         if (error) {
-            console.log("unable to connect to servers");
+            callback("unable to connect to servers");
         }
 
         else if (JSON.parse(body).status === 'ZERO_RESULTS') {
-            console.log("unable to find that address");
+            callback("unable to find that address");
 
 
         }
         else if (JSON.parse(body).status === 'OK') {
-            console.log(`address: ${JSON.parse(body).results[0].formatted_address}`);
-            console.log(`latitude: ${JSON.parse(body).results[0].geometry.location.lat}`);
-            console.log(`longitude: ${JSON.parse(body).results[0].geometry.location.lng}`);
-            console.log(`address: ${JSON.parse(body).results[0].formatted_address}`);
+            callback(undefined, {
+                address: JSON.parse(body).results[0].formatted_address,
+                latitude: JSON.parse(body).results[0].geometry.location.lat,
+                longitude: JSON.parse(body).results[0].geometry.location.lng
+
+            });
+
         }
     });
 };
