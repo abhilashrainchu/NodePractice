08205ad5b756be247c89f4a3ceac31ae312f4640
Abhilash Rainchu <abhilashrainchu@gmail.com>
2018-04-19 01:20:01 -0500

    code to retrieve latitude and longitude

--- weather-app/playground/app.js
+++ weather-app/playground/app.js
@@ -4,5 +4,6 @@ const request = require('request');
 request({url: "https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombardstreet%20philadelphia",
     JSON: true }, (error,response,body) => {
   console.log(`address: ${JSON.parse(body).results[0].formatted_address}`);
-});
-
+  console.log(`latitude: ${JSON.parse(body).results[0].geometry.location.lat}`);
+    console.log(`longitude: ${JSON.parse(body).results[0].geometry.location.lng}`);
+    });
