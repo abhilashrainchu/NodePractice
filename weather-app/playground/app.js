431527e20cde55db7a0959ab1a69eb574bd7cc05
skotha <skotha@meredith.com>
2018-04-19 13:06:22 -0500

    Parsing the body in if conditions

--- weather-app/playground/app.js
+++ weather-app/playground/app.js
@@ -1,14 +1,14 @@
 const request = require('request');
-const args  = require('yargs');
+const args = require('yargs');
 const argv = args
     .options({
-       a: {
-           demand: true,
-           alias: 'address',
-           describe: 'address to fetch weather for ',
-           string: true
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'address to fetch weather for ',
+            string: true
 
-       }
+        }
     })
     .help()
     //.alias('help', h )
@@ -17,23 +17,23 @@ const argv = args
 var encodeaddress = encodeURIComponent(argv.a);
 
 
-request({url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeaddress}`,
-    JSON: true }, (error,response,body) =>
-{
-        if (error) {
-            console.log("unable to connect to servers");
-        }
-        else if(body.status === 'ZERO_RESULTS') {
-            console.log("unable to find that address");
-            console.log("Abhul")
+request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeaddress}`,
+    JSON: true
+}, (error, response, body) => {
+    if (error) {
+        console.log("unable to connect to servers");
+    }
 
-        }
-        else if(body.status === 'OK') {
-            console.log(`address: ${JSON.parse(body).results[0].formatted_address}`);
-            console.log(`latitude: ${JSON.parse(body).results[0].geometry.location.lat}`);
-            console.log(`longitude: ${JSON.parse(body).results[0].geometry.location.lng}`);
-            console.log(`address: ${JSON.parse(body).results[0].formatted_address}`);
-        }
+    else if (JSON.parse(body).status === 'ZERO_RESULTS') {
+        console.log("unable to find that address");
+        console.log("Abhul")
 
-
-    });
+    }
+    else if (JSON.parse(body).status === 'OK') {
+        console.log(`address: ${JSON.parse(body).results[0].formatted_address}`);
+        console.log(`latitude: ${JSON.parse(body).results[0].geometry.location.lat}`);
+        console.log(`longitude: ${JSON.parse(body).results[0].geometry.location.lng}`);
+        console.log(`address: ${JSON.parse(body).results[0].formatted_address}`);
+    }
+});
